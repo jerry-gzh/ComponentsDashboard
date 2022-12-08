4ef30e34247b23cf5590fcfa53046ddb607560ce
Gerardo G <gerardoa.guzmanh@gmail.com>
2022-12-07 20:30:15 -0600

    Agregando rutas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Post from './components/API';
 import Swapi from './components/swapi'
 import MyCard from './components/MyCard'
 import SwapiBtn from './components/swapiButton'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
 
 function App() {
   return (
@@ -26,7 +28,22 @@ function App() {
       <>
         <SwapiBtn/>
       </>
-        <MyCard/>
+        {/* <MyCard/> */}
+      <Router>
+        <div className='Home'>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/MyCard">My Card</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/MyCard" element={<MyCard/>}/> 
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
